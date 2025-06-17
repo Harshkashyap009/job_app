dba1095434078a074d4b470dca44c5c92f82704d
Harshkashyap009 <kashyapp270.com>
2025-06-17 23:54:19 +0530

    Updated features / bug fixes

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -52,14 +52,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'job_portal.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jobportal',
-        'USER': 'postgres',
-        'PASSWORD': 'harsh123',
-        'HOST': 'localhost',
-        'PORT': '3333',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
