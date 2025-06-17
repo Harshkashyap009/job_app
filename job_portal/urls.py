972871d7acba60bc64d1e49651c562a48bb4ccdb
Harshkashyap009 <kashyapp270.com>
2025-06-18 00:57:53 +0530

    Add Procfile for Render deployment

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -28,8 +28,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('', include('jobs.urls')),
+    path('', home, name='home'),
+    
 
     # Job URLs
     path('employer/dashboard/', views.employer_dashboard, name='employer-dashboard'),
